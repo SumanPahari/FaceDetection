99258b64da1f69869bfac94f099c8c3fcb9375c0
sumanpahari <sumanpahari365@gmail.com>
2024-09-03 21:57:25 +0530

    add login

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Install python below 3.12 like 3.9 to 3.11
 
 
 # Step1 : install from terminal
-pip install fastapi pandas uvicorn aiofiles 
+pip install fastapi pandas uvicorn aiofiles python-multipart openpyxl
 # Step2 : Run services.py
 python services.py
 # Step3 : copy below line and paste it into browser and test the api
--- main.py
+++ main.py
@@ -1,7 +1,83 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+import pandas as pd
+import os
+import shutil
 
 app = FastAPI()
+UPLOAD_FOLDER = 'upload_image'
+
+
+
 
 @app.post("/getname")
 def read_root(name:str):
     return {"message": f"Hello, World {name}"}
+
+
+@app.post("/login")
+def read_root(username: str, password: str):
+    try:
+        df = pd.read_excel('userdata.xlsx')
+        print(f"LEN : {len(df)}")
+
+        # Filter the DataFrame to find the matching row
+        matching_row = df.loc[(df['USERNAME'] == username) & (df['PASSWORD'] == password)]
+        
+        # If a match is found, return the entire row
+        if not matching_row.empty:
+            return {'message': 'Login successful', 'row': matching_row.to_dict('records')[0]}
+        else:
+            return {'message': 'Invalid username or password'}
+    except Exception as e:
+        return {'message': f'An error occurred: {str(e)}'}
+
+@app.post("/uploadimage/")
+async def upload_image(file: UploadFile = File(...)):
+    try:
+        # Create a unique filename to avoid conflicts
+        file_location = os.path.join(UPLOAD_FOLDER, file.filename)
+
+        # Save the uploaded file to the specified directory
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        # Return a success message with the file location
+        return {"filename": file.filename, "location": file_location}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return JSONResponse(content={"message": "Image uploaded successfully"}, status_code=201)
+
+@app.post("/useridinfo")
+def read_root(useridinfo: str, lat: str, longi: str):
+    #try:
+    # Load the Excel file
+    df = pd.read_excel('userdata2.xlsx')
+    print(f"useridinfo :{useridinfo}")
+    ls=df['USERID'] == useridinfo
+    print(f"ls : {ls}")
+    print(df.head())
+
+    matching_row = df.loc[(df['USERID'] == useridinfo)]
+    print(f"matching_row : {matching_row.to_dict('records')}")
+    if not matching_row.empty:
+        # Update the latitude and longitude values
+        df.loc[(df['USERID'] == useridinfo), 'LATITUDE'] = lat
+        df.loc[(df['USERID'] == useridinfo), 'LONGITUDE'] = longi
+            
+        # Save the updated DataFrame to the Excel file
+        df.to_excel('userdata2.xlsx', index=False)
+        return {'message': 'if'}
+    else:
+        print(f"before :{len(df)}")
+        # Insert a new row into the DataFrame
+        df.loc[len(df)] = [useridinfo,lat, longi]
+        print(f"After :{len(df)}")
+        # Save the updated DataFrame to the Excel file
+        df.to_excel('userdata2.xlsx', index=False)
+                
+        return {'message': 'else'}
+    
+    
+    
